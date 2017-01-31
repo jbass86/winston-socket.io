c177caca9f363d5a214625c6a43e6b7bd553c375
Josh Bass <jbass86@gmail.com>
2017-01-30 21:43:16 -0500

    cleaned up some formatting and updated the default hostname to use the proper protocol

--- examples/client.js
+++ examples/client.js
@@ -2,7 +2,13 @@
 var winston = require("winston");
 require("../lib/winston-socketio");
 
-winston.add(winston.transports.SocketIO);
+winston.add(winston.transports.SocketIO, {
+  host: "http://localhost",
+  port: 3000,
+  reconnect: true,
+  namespace: "log",
+  log_topic: "log"
+});
 
 winston.log("info", "Yo Lets log to socket.io");
 winston.log("error", "Hey I logged an error");
