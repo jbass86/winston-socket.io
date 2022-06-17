153a1ed022fc2009666f2bdcad7d13a545c06caa
Josh Bass <jbass86@gmail.com>
2022-06-17 00:52:47 -0400

    fix the socket tests

--- test/winston-socket-test.js
+++ test/winston-socket-test.js
@@ -22,7 +22,7 @@ describe("standard winston socket test", function () {
       transports: [
         new winston.transports.Console(),
         new winston.transports.SocketIO({
-          host: "http://localhost",
+          host: "localhost",
           port: 3002,
           secure: false,
           reconnect: true,
@@ -62,53 +62,51 @@ describe("standard winston socket test", function () {
 
 describe("encrypted winston socket test", function () {
 
-beforeEach(function (done) {
-logger = winston.createLogger({
-level: "info",
-transports: [
-new winston.transports.Console(),
-new winston.transports.SocketIO(
-{
-host: "http://localhost",
-port: 3001,
-secure: false,
-encrypt: true,
-secret: "secret",
-reconnect: true,
-log_topic: "log_encrypted"
-}
-)
-]
-});
-logger.log("info", "I'm logging encrypted message to the socket.io server!!!");
-done();
-});
+  beforeEach(function (done) {
+    logger = winston.createLogger({
+    level: "info",
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.SocketIO({
+        host: "localhost",
+        port: 3001,
+        secure: false,
+        encrypt: true,
+        secret: "secret",
+        reconnect: true,
+        log_topic: "log_encrypted"
+      })
+    ]
+    });
+    logger.log("info", "I'm logging encrypted message to the socket.io server!!!");
+    done();
+  });
 
-it("recieves encrypted logs from winston and decrypts them", function (done) {
-server.listen(3001, "localhost");
-io.use(encrypt("secret"));
-io.on("connection", function (socket) {
-socket.on("log_encrypted", function (data) {
-JSON.stringify(data).should.equal("{\"level\":\"info\",\"message\":\"I'm logging encrypted message to the socket.io server!!!\"}");
-done();
-});
+  it("recieves encrypted logs from winston and decrypts them", function (done) {
+    server.listen(3001, "localhost");
+    io.use(encrypt("secret"));
+    io.on("connection", function (socket) {
+    socket.on("log_encrypted", function (data) {
+    JSON.stringify(data).should.equal("{\"level\":\"info\",\"message\":\"I'm logging encrypted message to the socket.io server!!!\"}");
+      done();
+    });
 
-});
+    });
 
-});
+  });
 
-after(async () => {
-var clients = {}
-await io.on('connection', function (socket) {
-clients[socket.id] = socket;
+  after(async () => {
+    const clients = {}
+    await io.on('connection', function (socket) {
+      clients[socket.id] = socket;
 
-socket.on('disconnect', function () {
-delete clients[socket.id];
-});
-});
-await server.close();
-await io.close();
-await logger.close();
-});
+      socket.on('disconnect', function () {
+        delete clients[socket.id];
+      });
+    });
+    await server.close();
+    await io.close();
+    await logger.close();
+  });
 
 });
